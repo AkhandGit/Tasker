d05fb9c25e4efba2b5a92c6fc36fa9547f4a36ea
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-04-20 19:22:26 +0530

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className="logo">
             <span className='font-bold text-xl mx-8'>iTask</span>
         </div>
-      <ul className="flex gap-7 mx-9">
+      <ul className="flex gap-8 mx-9">
         <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
         <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
       </ul>
